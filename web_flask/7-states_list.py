c044d9d645c30e3a3af06d9e6783494be3e1c875
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-04-10 11:48:14 +0200

    Fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Starts a Flask web application."""
 from models import storage
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -19,4 +18,4 @@ def teardown(exc):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
